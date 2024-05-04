783201f4c157819dca92da6c84af4b5f79d26887
Rakesh Sharma <62244892+Xythercoder@users.noreply.github.com>
2024-05-04 19:13:52 +0530

    done

--- inventoryfd/src/index.js
+++ inventoryfd/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.min.js';
+import '../src/assets/css/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from '../src/utils/store';
